be9dd59e02af410ec027f24411d37d8ccf70981d
Dominic Francis <106940227+DF-0P@users.noreply.github.com>
2022-08-05 02:15:28 +0100

    Update basic order software program..final.py

--- basic order software program..final.py
+++ basic order software program..final.py
@@ -23,20 +23,19 @@ def kkkk():
             ab = resources.get(a)[1]
             pieces(a, ab)
         elif (a == "r"):
-            print(menu)
-            print(resources)
-            print("")
-        else: #else statement to grab the unknown value gotten
-            #pass #used this to start afresh the program initially till i made the line below
-            #finish() #used this to print the finish product of their order incase they intended terminating the program , but took it out to make the line below
+            new = []
+            for idx, i in enumerate(range(len(menu)), start=1):
+                new.append({"index": idx, "Name": resources[f'{i+1}'][1], "Price": menu.get(resources.get(f'{i+1}')[1]), "NO. Available": resources[f'{i+1}'][0]})
+            print(pd.DataFrame(new))
+        else: 
             print("Please choose the number of the menu item\n")
             kkkk()
             
 
 def pieces(a, ab):
             b = input("how many pieces?: ")
-            try: #try state to run
-                assert b in str(list(range(100))) #this is to ensure that it's a number thats given and not a letter or character
+            try: 
+                assert b in str(list(range(100))) 
                 total_price = 0
                 b = int(b)
                 if b > resources.get(a)[0]:
@@ -44,11 +43,11 @@ def pieces(a, ab):
                     finish()
                 else:
                     print(total_price)
-                    pr.append(menu.get(ab) * b), sr.append(b), fr.append(ab) #this reduces the lines of code needed
-                    resources[a][0] = (resources.get(a)[0]- b) #this appends the new value for the amount of the particular order made
-                    answer = input("do you want to add an order?(y/n): ") #question to run to ask whether the want it to run again or not
-                    kkkk() if answer == "y" else finish() #this is a tenary statement to run depending on the answer
-            except: #except statement to catch the assertion error
+                    pr.append(menu.get(ab) * b), sr.append(b), fr.append(ab) 
+                    resources[a][0] = (resources.get(a)[0]- b)
+                    answer = input("do you want to add an order?(y/n): ") 
+                    kkkk() if answer == "y" else finish() 
+            except: 
                 print("Input is not a valid Number.\nTry again.\n")
                 pieces(a, ab)
 
@@ -56,5 +55,5 @@ while True:
     print("Welcome to Cloud Chicken Service\nwhat would you like to eat:\n1. chicken burger $20\n2. smoothy $10\n3. meat pie $15")
     kkkk()
     print("Thank you for Shopping at Cloud Chicken Service.\nYour satisfaction is our concern.\nSee you Soon.\nProgram finished\n")
-    pr, sr, fr= [], [], [] #pr = customer choice price, sr = number ordered, fr = choice of food from menu  #THEIR RESETS
+    pr, sr, fr= [], [], [] 
     time.sleep(3)
